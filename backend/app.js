102c4cebbefecd7b4b9cd89807151432592add0e
Jedrzej <jedrekknapik@gmail.com>
2024-01-21 18:22:14 +0100

    feat(backend): add create new product endpoint

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,20 @@ import dotenv from "dotenv";
 import { connectDatabase } from "./config/dbConnect.js";
 
 const app = express();
-dotenv.config({path: "backend/config/config.env"})
+dotenv.config({ path: "backend/config/config.env" });
 
 // Connecting to db
 connectDatabase();
+
+// Use JSON request body
+app.use(express.json());
 
 // Import all routes
 import productRoutes from "./routes/products.js";
 app.use("/api/v1", productRoutes);
 
-
 app.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT} is ${process.env.NODE_ENV} mode.`)
-})
+  console.log(
+    `Server started on port ${process.env.PORT} is ${process.env.NODE_ENV} mode.`
+  );
+});
--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,5 +1,17 @@
+import Product from "../models/product.js";
+
 export const getProducts = async (req, res) => {
-    res.status(200).json({
-        message: "All products"
-    });
-}
+  const products = await Product.find();
+
+  res.status(200).json({
+    products,
+  });
+};
+
+export const newProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+
+  res.status(200).json({
+    product,
+  });
+};
